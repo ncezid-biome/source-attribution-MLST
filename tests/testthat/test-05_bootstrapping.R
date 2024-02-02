cfa097500816ff8a721fd79ce65f8a32dcf4b543
Sean Browning <25888380+beansrowning@users.noreply.github.com>
2024-02-02 10:22:47 -0500

    Re-working unittests - Under the principle that each test should not depend on external dependencies and be able to run in isolation - Removing package imports which aren't neeeded

--- tests/testthat/test-05_bootstrapping.R
+++ tests/testthat/test-05_bootstrapping.R
@@ -1,43 +1,35 @@
-suppressPackageStartupMessages(library("ape"))
-suppressPackageStartupMessages(library("cluster"))
-suppressPackageStartupMessages(library("data.table"))
-suppressPackageStartupMessages(library("dendextend"))
-suppressPackageStartupMessages(library("dplyr"))
-suppressPackageStartupMessages(library("logger"))
-suppressPackageStartupMessages(library("gt"))
-suppressPackageStartupMessages(library("randomForestSRC"))
-suppressPackageStartupMessages(library("tidyverse"))
-
-log_threshold(SUCCESS)
 
 test_that("Bootstrapping on LMO0003", {
-
-  input  <- sourcerer::Listeria_isolates 
-  output <- rds_dir
+  input <- system.file("extdata/Listeria_isolates.csv.gz", pacakge = "sourcerer")
+  output <- tempdir()
   ncores <- 1
   bootstrap_reps <- 3
   loci_start_with <- "LMO0003"
   my_seed <- 23
-  print(paste0("=====I will read csv file ",input, "======"))
-  filenames <- bootstrapping(input = input, output = output, 
-                  ncores = ncores, bootstrap_reps = bootstrap_reps,
-                  loci_start_with = loci_start_with, my_seed = my_seed )
 
-  #filenames <- bootstrapping(opt)
+  filenames <- bootstrapping(
+    input = input, output = output,
+    ncores = ncores, bootstrap_reps = bootstrap_reps,
+    loci_start_with = loci_start_with, my_seed = my_seed
+  )
 
-  expect_equal(basename(filenames[[1]]), 
-      basename("test-results/bs23.rds"), info = "Filename 1")
-  expect_equal(basename(filenames[[2]]), 
-      basename("test-results/bs24.rds"), info = "Filename 2")
-  expect_equal(basename(filenames[[3]]), 
-      basename("test-results/bs25.rds"), info = "Filename 3")
+  expect_equal(basename(filenames[[1]]),
+    "bs23.rds",
+    info = "Filename 1"
+  )
+  expect_equal(basename(filenames[[2]]),
+    "bs24.rds",
+    info = "Filename 2"
+  )
+  expect_equal(basename(filenames[[3]]),
+    "bs25.rds",
+    info = "Filename 3"
+  )
 
 
-  rf      <- list()
+  rf <- list()
   rf[[1]] <- readRDS(filenames[[1]])
   expect_equal(rf[[1]]$forest$n, 719, info = "Number of alleles in the first RF model")
   expect_equal(rf[[1]]$forest$yvar.names, "food", info = "yvar in the first RF model")
   expect_equal(rf[[1]]$importance["LMO00030", "all"], 0.07586199, tolerance = 0.01, info = "importance of LMO00030 in the all category")
-
 })
-
--- tests/testthat/test-10_prediction.R
+++ tests/testthat/test-10_prediction.R
@@ -1,109 +1,102 @@
-suppressPackageStartupMessages(library("cluster"))
-suppressPackageStartupMessages(library("ape"))
-suppressPackageStartupMessages(library("cluster"))
-suppressPackageStartupMessages(library("data.table"))
-suppressPackageStartupMessages(library("dendextend"))
-suppressPackageStartupMessages(library("dplyr"))
-suppressPackageStartupMessages(library("logger"))
-suppressPackageStartupMessages(library("gt"))
-suppressPackageStartupMessages(library("randomForestSRC"))
-suppressPackageStartupMessages(library("tidyverse"))
-
-log_threshold(SUCCESS)
 
 test_that("Prediction on LMO0003 with example_query", {
+  input <- system.file("extdata/Listeria_isolates.csv.gz", pacakge = "sourcerer")
+  output <- tempdir()
   ncores <- 1
+  bootstrap_reps <- 3
+  loci_start_with <- "LMO0003"
+  my_seed <- 23
+
+  filenames <- bootstrapping(
+    input = input, output = output,
+    ncores = ncores, bootstrap_reps = bootstrap_reps,
+    loci_start_with = loci_start_with, my_seed = my_seed
+  )
 
   # rfsrc prediction objects
   predictions <- list()
-  for(i in seq(1,3)){
-    model <- paste0(rds_dir,"/bs",(i+22),".rds")
-    predictions[[i]] <- prediction(model_filename = model, 
-                                   query = example_query, ncores = ncores)
-    
-    
-    # Save this file for downstream tests
-    out_file <- paste0(rds_dir, "/predictions", (i+22), ".rds")
-    #log_success(paste0("Saving file to ", out_file))
-    saveRDS(predictions[[i]], file = out_file)
-    #log_success("BREAK to quickly dev downstream"); break;
+
+  for (i in seq_along(filenames)) {
+    model <- filenames[i]
+    predictions[[i]] <- prediction(
+      model_filename = model,
+      query = example_query, ncores = ncores
+    )
   }
 
   # Test on the first prediction
-  pred <- readRDS(paste0(rds_dir, "/predictions23.rds"))
+  pred <- predictions[[1]]
   my_table <- pred$predicted
 
   expect_equal(sort(colnames(my_table)), sort(c("dairy", "meat", "vegetable", "fruit", "seafood")), expected.label = "column names on the prediction table")
 
   # Dairy
-  obs <- round(my_table[,"dairy"],3) + 0
-  exp <- round(0.1508644,3) + 0
+  obs <- round(my_table[, "dairy"], 3) + 0
+  exp <- round(0.1508644, 3) + 0
   names(obs) <- "dairy"
   names(exp) <- "dairy"
 
   expect_equal(
-               obs,
-               exp,
-               info = "Test for dairy percentage",
-               label = paste0("Observed dairy percentage (", obs, ")"),
-               expected.label = paste0("Expected dairy percentage (", exp, ")")
-               )
+    obs,
+    exp,
+    info = "Test for dairy percentage",
+    label = paste0("Observed dairy percentage (", obs, ")"),
+    expected.label = paste0("Expected dairy percentage (", exp, ")")
+  )
 
   # Meat
-  obs <- round(my_table[,"meat"],3) + 0
-  exp <- round(0.3109127,3) + 0
+  obs <- round(my_table[, "meat"], 3) + 0
+  exp <- round(0.3109127, 3) + 0
   names(obs) <- "meat"
   names(exp) <- "meat"
 
   expect_equal(
-               obs,
-               exp,
-               info = "Test for meat percentage",
-               label = paste0("Observed meat percentage (", obs, ")"),
-               expected.label = paste0("Expected meat percentage (", exp, ")")
-               )
+    obs,
+    exp,
+    info = "Test for meat percentage",
+    label = paste0("Observed meat percentage (", obs, ")"),
+    expected.label = paste0("Expected meat percentage (", exp, ")")
+  )
 
   # Vegetable
-  obs <- round(my_table[,"vegetable"],3) + 0
-  exp <- round(0.2108486,3) + 0
+  obs <- round(my_table[, "vegetable"], 3) + 0
+  exp <- round(0.2108486, 3) + 0
   names(obs) <- "vegetable"
   names(exp) <- "vegetable"
 
   expect_equal(
-               obs,
-               exp,
-               info = "Test for vegetable percentage",
-               label = paste0("Observed vegetable percentage (", obs, ")"),
-               expected.label = paste0("Expected vegetable percentage (", exp, ")")
-               )
+    obs,
+    exp,
+    info = "Test for vegetable percentage",
+    label = paste0("Observed vegetable percentage (", obs, ")"),
+    expected.label = paste0("Expected vegetable percentage (", exp, ")")
+  )
 
   # Fruit
-  obs <- round(my_table[,"fruit"],3) + 0
-  exp <- round(0.1569874,3) + 0
+  obs <- round(my_table[, "fruit"], 3) + 0
+  exp <- round(0.1569874, 3) + 0
   names(obs) <- "fruit"
   names(exp) <- "fruit"
 
   expect_equal(
-               obs,
-               exp,
-               info = "Test for fruit percentage",
-               label = paste0("Observed fruit percentage (", obs, ")"),
-               expected.label = paste0("Expected fruit percentage (", exp, ")")
-               )
+    obs,
+    exp,
+    info = "Test for fruit percentage",
+    label = paste0("Observed fruit percentage (", obs, ")"),
+    expected.label = paste0("Expected fruit percentage (", exp, ")")
+  )
 
   # Seafood
-  obs <- round(my_table[,"seafood"],3) + 0
-  exp <- round(0.1703869,3) + 0
+  obs <- round(my_table[, "seafood"], 3) + 0
+  exp <- round(0.1703869, 3) + 0
   names(obs) <- "seafood"
   names(exp) <- "seafood"
 
   expect_equal(
-               obs,
-               exp,
-               info = "Test for seafood percentage",
-               label = paste0("Observed seafood percentage (", obs, ")"),
-               expected.label = paste0("Expected seafood percentage (", exp, ")")
-               )
-
+    obs,
+    exp,
+    info = "Test for seafood percentage",
+    label = paste0("Observed seafood percentage (", obs, ")"),
+    expected.label = paste0("Expected seafood percentage (", exp, ")")
+  )
 })
-
